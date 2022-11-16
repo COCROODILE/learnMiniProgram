b7f2e98713485bde4efd5073d1991729e0ef04b4
COCROODILE <3082897304@qq.com>
2022-11-16 11:37:40 +0800

    事件

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,66 +1,44 @@
 // pages/home/home.js
 Page({
 
-    /**
-     * 页面的初始数据
-     */
-    data: {
+	data: {
+		titles:['衣服','裤子','帽子']
+	},
 
-    },
+	handleTouchStart(){
+		console.log('handleTouchStart');
+	},
 
-    /**
-     * 生命周期函数--监听页面加载
-     */
-    onLoad(options) {
+	handleEventClick(event){
+		console.log(event);
+	},
+	handleEventEnd(event){
+		console.log('+++++++',event);
+	},
+	handleItemClick(event){
+		console.log(event);
+		const dataset=event.currentTarget.dataset
+		const item=dataset.item
+		const index=dataset.index
+		console.log(item,index);
+	},
 
-    },
-
-    /**
-     * 生命周期函数--监听页面初次渲染完成
-     */
-    onReady() {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面显示
-     */
-    onShow() {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面隐藏
-     */
-    onHide() {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面卸载
-     */
-    onUnload() {
-
-    },
-
-    /**
-     * 页面相关事件处理函数--监听用户下拉动作
-     */
-    onPullDownRefresh() {
-
-    },
-
-    /**
-     * 页面上拉触底事件的处理函数
-     */
-    onReachBottom() {
-
-    },
-
-    /**
-     * 用户点击右上角分享
-     */
-    onShareAppMessage() {
-
-    }
+	handleCaptureBox1(){
+		console.log('handleCaptureBox1');
+	},
+	handleBindBox1(){
+		console.log('handleBindBox1');
+	},
+	handleCaptureBox2(){
+		console.log('handleCaptureBox2');
+	},
+	handleBindBox2(){
+		console.log('handleBindBox2');
+	},
+	handleCaptureBox3(){
+		console.log('handleCaptureBox3');
+	},
+	handleBindBox3(){
+		console.log('handleBindBox3');
+	}
 })
